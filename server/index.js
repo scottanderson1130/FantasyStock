1f89b86262dab4151e1f4ce81353e649aad2636b
Scott Anderson <scott.anderson1130@gmail.com>
2020-10-23 14:49:28 -0500

    setup/update to server file to solve 404 error

--- server/index.js
+++ server/index.js
@@ -22,11 +22,7 @@ const PORT = process.env.SERVER_PORT || 8080;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded())
-// app.use(express.static(DIST_DIR));
+app.use(express.static(DIST_DIR));
 
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
