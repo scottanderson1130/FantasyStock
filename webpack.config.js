33c560a642fd4a16210e1d26c1461edf5ef4b2fb
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-23 15:28:28 -0500

    update/webpack and server index to accept react routes

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
+    publicPath: '/'
   },
   plugins: [htmlPlugin],
   resolve: {
@@ -32,4 +33,7 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+  }
 };
