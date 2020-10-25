e3f54736e2b91c812e53bb15c1fef9e092b8f596
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-25 00:15:19 -0500

    (setup) League Model

---
+++ server/db/models/League.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const League = sequelize.define('league', {
+    league_name: {
+      type: DataTypes.STRING,
+    },
+    settings: {
+      type: DataTypes.JSON,
+    },
+  }, {
+    freezeTableName: true,
+  });
+
+  League.associate = (models) => {
+    League.belongsToMany(models.User, {
+      through: models.League_user,
+      foreignKey: 'id_league',
+    });
+    League.belongsTo(models.User, {
+      foreignKey: 'id_user',
+    });
+  };
+  return League;
+};
