b651d34b2f5a4ac1fe7a4e7bd3a7784a2af2bb3f
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-25 19:58:16 -0500

    (add) Waivers search bar with table functionality

--- src/components/WaiversTable.jsx
+++ src/components/WaiversTable.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -10,28 +11,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
 import '../css/BasicTable.css';
-
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name, calories, fat, carbs, protein
-  };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0)
-];
+import { TablePagination } from '@material-ui/core';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -159,16 +139,32 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function WaiversTable() {
+// eslint-disable-next-line react/prop-types
+function WaiversTable({ rows, search }) {
   const classes = useStyles();
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('calories');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(15);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // eslint-disable-next-line react/prop-types
+  // eslint-disable-next-line max-len
+  const filteredStocks = rows.filter((row) => row.name.toLowerCase().indexOf(search.toLowerCase()) !== -1);
 
   return (
     <div className={classes.root}>
@@ -187,7 +183,8 @@ function WaiversTable() {
               rowCount={rows.length}
             />
             <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
+              {stableSort(filteredStocks, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
                   const labelId = `enhanced-table-checkbox-${index}`;
 
@@ -213,6 +210,15 @@ function WaiversTable() {
             </TableBody>
           </Table>
         </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 15, 30]}
+          component='div'
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
       </Paper>
     </div>
   );
--- src/views/Waivers.jsx
+++ src/views/Waivers.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField } from '@material-ui/core';
 import WaiversTable from '../components/WaiversTable.jsx';
+import '../css/Waivers.css';
 
 function Waivers() {
+  const [search, setSearch] = useState('');
+
+  const rows = [
+    {
+      name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3
+    },
+    {
+      name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9
+    },
+    {
+      name: 'Eclair', calories: 262, fat: 16.0, carbs: 24, protein: 6.0
+    },
+    {
+      name: 'Frozen yoghurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0
+    },
+    {
+      name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9
+    },
+    {
+      name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5
+    },
+    {
+      name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3
+    },
+    {
+      name: 'Jelly Bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0
+    },
+    {
+      name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7.0
+    },
+    {
+      name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0.0
+    },
+    {
+      name: 'Marshmallow', calories: 318, fat: 0, carbs: 81, protein: 2.0
+    },
+    {
+      name: 'Nougat', calories: 360, fat: 19.0, carbs: 9, protein: 37.0
+    },
+    {
+      name: 'Oreo', calories: 437, fat: 18.0, carbs: 63, protein: 4.0
+    }
+  ];
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // console.log(InputProps);
+
   return (
-    <div>
-      <h1>Waivers</h1>
-      <WaiversTable />
+
+    <div className='waivers'>
+      <div className='waivers_search'>
+        <TextField
+          style={{ backgroundColor: 'white' }}
+          freesolo='true'
+          type='search'
+          label='Search Stock'
+          margin='normal'
+          variant='outlined'
+          onChange={handleSearch}
+          size='small'
+        />
+      </div>
+      <WaiversTable rows={rows} search={search} />
     </div>
   );
 }
