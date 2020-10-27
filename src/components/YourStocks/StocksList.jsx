e12ac677ca6f98c5f027f4a30b88ea2adbc6f919
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-27 17:36:40 -0500

    (refactor) component files into separate folders

---
+++ src/components/YourStocks/StocksList.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable react/prop-types */
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TableCell,
+  TableRow,
+  TextField
+} from '@material-ui/core';
+import React, { useState } from 'react';
+// import WaiversPopUp from './WaiversPopUp.jsx';
+
+function StocksList({ row, index }) {
+  const [open, setOpen] = useState(false);
+  const [myStocks, setMyStocks] = useState({});
+
+  const handleOpen = () => {
+    setMyStocks(row);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <TableRow
+        className='basicTable_row'
+        hover
+        role='checkbox'
+        tabIndex={-1}
+        key={index}
+      >
+        <TableCell padding='checkbox' />
+        <TableCell onClick={handleOpen} component='th' id={index} scope='row' padding='none'>
+          {row.name}
+        </TableCell>
+        <TableCell align='right'>{row.calories}</TableCell>
+        <TableCell align='right'>{row.fat}</TableCell>
+        <TableCell align='right'>{row.carbs}</TableCell>
+        <TableCell align='right'>{row.protein}</TableCell>
+      </TableRow>
+      <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
+        <DialogTitle id='form-dialog-title'>Stock Details</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {myStocks.name}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin='dense'
+            id='name'
+            label='something'
+            type='email'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin='dense'
+            id='name'
+            label='something'
+            type='email'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin='dense'
+            id='name'
+            label='something'
+            type='email'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin='dense'
+            id='name'
+            label='something'
+            type='email'
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary'>
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color='primary'>
+            Update
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default StocksList;
