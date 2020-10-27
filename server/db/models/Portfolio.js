7d59ab07d346f2ce556017836ff036c32eee2647
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-27 01:58:10 -0500

    (setup) Portfolio Model

---
+++ server/db/models/Portfolio.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Portfolio = sequelize.define('portfolio', {
+    shares: {
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
+    price_per_share_at_purchase: {
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
+  }, {
+    freezeTableName: true,
+  });
+  Portfolio.associate = (models) => {
+    Portfolio.belongsTo(models.League, {
+      foreignKey: 'id_league',
+    });
+  };
+
+  return Portfolio;
+};
