fb4d4206cc284b070c1254e98e053fb52fc17b02
Scott Anderson <scott.anderson1130@gmail.com>
2020-10-28 13:02:03 -0500

    (add) News Articles to Homepage

---
+++ src/components/Home/StockList.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    paddingRight: '20px',
+    margin: '10px'
+  },
+  button: {
+    float: 'right'
+  }
+});
+
+function StockList({ article }, key) {
+  // eslint-disable-next-line react/prop-types
+  const {
+    author, title, url, urlToImage, description, publishedAt
+  } = article;
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid item xs={12}>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              alt={author}
+              height='140'
+              image={urlToImage}
+              title='Contemplative Reptile'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='h2'>
+                {title}
+                {' '}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                <em>{new Date(publishedAt).toDateString()}</em>
+                <br />
+                {description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.button}>
+            <Button variant='outlined' size='small' color='primary' href={url}>
+              Link
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </div>
+  );
+}
+
+export default StockList;
