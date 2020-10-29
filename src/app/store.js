3fb55190f7b1af70870cd30ad3c29b7c8f407bcb
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-29 13:07:08 -0500

    (add) Redux state for waivers

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice.js';
+import waiversReducer from '../features/waiversSlice.js';
 
 export default configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    waivers: waiversReducer
   }
 });
