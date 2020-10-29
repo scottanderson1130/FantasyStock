c4e2b90228409f14800a78129410f42fafb57d95
Scott Anderson <scott.anderson1130@gmail.com>
2020-10-29 16:41:51 -0500

    (update) News call to always use current date

--- src/components/Home/Stocknews.jsx
+++ src/components/Home/Stocknews.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import StockList from './StockList.jsx';
-import fakenews from './fakenews.json';
+// import fakenews from './fakenews.json';
 
-// const config = {
-//   method: 'get',
-//   url: 'https://newsapi.org/v2/everything?q=stocks&from=2020-10-28&to=2020-10-28&sortBy=popularity',
-//   headers: {
-//     'X-API-Key': 'b56b4a9004a848c78d1b0fd02bbf2ad0',
-//     Cookie: '__cfduid=d15d8964d6c38c18e9524dc8b4843c9241603756878'
-//   }
-// };
+const todayDate = new Date().toISOString().split('T')[0];
+
+const config = {
+  method: 'get',
+  url: `https://newsapi.org/v2/everything?q=stocks&from=${todayDate}&to=${todayDate}&sortBy=popularity`,
+  headers: {
+    'X-API-Key': 'b56b4a9004a848c78d1b0fd02bbf2ad0',
+    Cookie: '__cfduid=d15d8964d6c38c18e9524dc8b4843c9241603756878'
+  }
+};
 function Stocknews() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    // axios(config)
-    //   .then((response) => {
-    //     setArticles(response.data.articles);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-    setArticles(fakenews.articles);
+    axios(config)
+      .then((response) => {
+        setArticles(response.data.articles);
+      })
+      .catch((error) => (error));
+    // setArticles(fakenews.articles);
   }, []);
 
   return (
