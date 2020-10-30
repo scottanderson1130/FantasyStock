7c878689b06526b2c8275342b37b8b1b841036ac
Scott Anderson <scott.anderson1130@gmail.com>
2020-10-30 10:09:26 -0500

    (update) Styled the card with the score

--- src/components/Home/MatchupCard.jsx
+++ src/components/Home/MatchupCard.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
-    textAlign: 'left',
+    diplay: 'flex',
     borderRadius: '25px'
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
   },
   title: {
     fontSize: 14
@@ -36,13 +31,13 @@ function MatchupCard() {
           League Name
         </Typography>
         <Typography variant='h5' component='h2'>
-          Team Name
+          Team Name 100
         </Typography>
         <Typography className={classes.pos} color='textSecondary'>
           Owner | 4-1
         </Typography>
         <Typography variant='h5' component='h2'>
-          Team Name
+          Team Name 90
         </Typography>
         <Typography className={classes.pos} color='textSecondary'>
           Owner | 5-0
