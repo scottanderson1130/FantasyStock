9bec6f0544476d2b9400cead76c2fa321ff264ef
Scott Anderson <scott.anderson1130@gmail.com>
2020-11-02 18:31:39 -0600

    (update) Toggle through differnt leagues in YourStocks

--- src/components/Home/MatchupCard.jsx
+++ src/components/Home/MatchupCard.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import { Link } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLeague } from '../../features/leagueSlice.js';
 
 const useStyles = makeStyles({
   root: {
@@ -22,29 +25,33 @@ const useStyles = makeStyles({
     marginTop: '10px'
   }
 });
-function MatchupCard() {
+function MatchupCard({ league, user }) {
+  const dispatch = useDispatch();
+
+  const clickLeagueUpdate = () => {
+    console.log(league.id_league);
+    dispatch(setLeague(league.id_league));
+  };
+
   const classes = useStyles();
   return (
     <Card className={classes.root} variant='outlined'>
       <CardContent>
         <Typography className={classes.title} color='textSecondary' gutterBottom>
-          League Name
+          {league.team_name}
         </Typography>
         <Typography variant='h5' component='h2'>
-          Team Name 100
+          {`${league.id_league} 100`}
         </Typography>
         <Typography className={classes.pos} color='textSecondary'>
-          Owner | 4-1
+          {`${user.username} | ${league.record}`}
         </Typography>
-        <Typography variant='h5' component='h2'>
-          Team Name 90
-        </Typography>
-        <Typography className={classes.pos} color='textSecondary'>
-          Owner | 5-0
-        </Typography>
+
       </CardContent>
       <CardActions style={{ justifyContent: 'center' }}>
-        <Button size='small' href='/yourstocks'>My Stocks</Button>
+      <Link key='leaguinfo' to='/yourstocks'>
+        <Button onClick={clickLeagueUpdate} size='small' href=''>My Stocks</Button>
+      </Link>
       </CardActions>
     </Card>
   );
