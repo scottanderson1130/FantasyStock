1884b3323c8488cecd6a7c94b01bcd54046d58a8
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-03 09:37:59 -0600

    (add) route for create and find league by id

--- server/routes/league.js
+++ server/routes/league.js
@@ -6,17 +6,55 @@ const { Router } = require('express');
 const leagueRouter = Router();
 
 const {
-  League
+  League,
+  League_user
 } = require('../db/index');
 
+// create a league route
 leagueRouter.post('/', (req, res) => {
   const { league_name, id_owner } = req.body;
 
   League.create({
     league_name,
     id_owner
-  }).then(() => {
-    res.send('created');
+  }).then((leagueInfo) => {
+    const responseLeagueInfo = { ...leagueInfo.dataValues };
+    League_user.create({
+      id_user: responseLeagueInfo.id_owner,
+      id_league: responseLeagueInfo.id
+    });
+    res.send(responseLeagueInfo);
+  })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+// league by id
+leagueRouter.post('/:leagueID', (req, res) => {
+  const { leagueID } = req.params;
+  const { id_owner } = req.body;
+
+  League.findOne({
+    where: {
+      id: leagueID,
+      id_owner
+    }
+  }).then((league) => {
+    const responseLeague = { ...league.dataValues };
+    League_user.findOne({
+      where: {
+        id_league: leagueID
+      }
+    })
+      .then((leagueInfo) => {
+        responseLeague.leagueUser = leagueInfo;
+        res.send(responseLeague);
+      });
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send(err);
   });
 });
 
