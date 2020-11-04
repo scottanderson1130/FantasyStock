c4c9c92494d2945f40a0fd10f083cc10a8666ebd
Scott Anderson <scott.anderson1130@gmail.com>
2020-11-04 17:07:19 -0600

    (update) Add file for messages routes

---
+++ server/routes/messages.js
@@ -0,0 +1,13 @@
+// const { Router } = require('express');
+// const { Message } = require('../db/index');
+
+// const messageRouter = Router();
+
+// messageRouter.get('/:leagueID', (req, res) => {
+// });
+
+// messageRouter.post('/:leagueID', (req, res) => {
+//   const { leagueID } = req.params;
+//   Message.findBy
+// });
+// messageRouter.put('/:leagueID', (req, res) => {});
