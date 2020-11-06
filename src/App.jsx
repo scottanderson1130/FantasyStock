58f913c328be8cdcf09321d63373712e8025a301
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-06 17:27:04 -0600

    (add) All user leagues to Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ScoreBoard from './views/ScoreBoard.jsx';
 import YourStocks from './views/YourStocks.jsx';
 import Waivers from './views/Waivers.jsx';
@@ -12,12 +12,14 @@ import TickerBar from './components/TickerBar.jsx';
 import LeagueInfo from './views/LeagueInfo.jsx';
 import Settings from './views/Settings.jsx';
 import MessageBoard from './views/MessageBoard.jsx';
-import { setLogIn, setUser } from './features/userSlice.js';
+import { selectUser, setLogIn, setUser } from './features/userSlice.js';
+import { setUserLeagues } from './features/leagueSlice.js';
 
 function App() {
   const logIn = true;
-
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const [userLeagueInfo, setUserLeagueInfo] = useState([]);
 
   useEffect(() => {
     async function fetchUser() {
@@ -33,13 +35,23 @@ function App() {
     fetchUser();
   }, [dispatch]);
 
+  useEffect(() => {
+    async function fetchUserLeagueInfo() {
+      const response = await axios.get(`/league/${user?.id}`);
+      setUserLeagueInfo(response.data[0].leagues);
+      dispatch(setUserLeagues(response.data[0].leagues));
+      return response;
+    }
+    fetchUserLeagueInfo();
+  }, [user, dispatch]);
+
   return (
     <Router>
       <div className='app'>
         <Nav logIn={logIn} />
         <TickerBar logIn={logIn} />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={Home} info={userLeagueInfo} />
           <Route path='/scoreboard' component={ScoreBoard} />
           <Route path='/yourstocks' component={YourStocks} />
           <Route path='/waivers' component={Waivers} />
