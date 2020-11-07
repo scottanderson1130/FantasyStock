397bb1aba37e01c3ba7240cc18fcfecb5c697ec4
Christopher Stumpe <toph.stumpe@gmail.com>
2020-11-07 11:19:55 -0600

    (update) league route

--- server/routes/league.js
+++ server/routes/league.js
@@ -12,25 +12,24 @@ const {
 leagueRouter.post('/', (req, res) => {
   const { league_name, id_owner, settings } = req.body;
   // Settings Template:
-// settings: {
-//   number_teams: integer
-//   length_matches: integer (number of days) (defaulting to 7)
-//   number_matches: integer
-//   start_date: date /defaults: next monday '''''' calculate
-//   end_date: date / it follows
-//   number_teams_playoffs: Integer / default top 40%, will be specific to the numbers options
-//   starting_bank: Integer / default 10,000,00 (remember extra )
-//   schedule: {
-       // currentWeek: val?,
-        //  Weekly matchup []
-// }
-// }
-  
-//where do i do schedule?
-// on league post? Shuffle team number
-// apply to standard
-// can build shuffler later
-// what is the format of the schedule aspect?
+  // settings: {
+  //   number_teams: integer
+  //   length_matches: integer (number of days) (defaulting to 7)
+  //   number_matches: integer
+  //   start_date: date /defaults: next monday '''''' calculate
+  //   end_date: date / it follows
+  //   number_teams_playoffs: Integer / default top 40%, will be specific to the numbers options
+  //   starting_bank: Integer / default 10,000,00 (remember extra )
+  //   schedule: {
+  // currentWeek: val?,
+  //  Weekly matchup []
+  // }
+  // }
+  // where do i do schedule?
+  // on league post? Shuffle team number
+  // apply to standard
+  // can build shuffler later
+  // what is the format of the schedule aspect?
   League.create({
     league_name,
     id_owner,
