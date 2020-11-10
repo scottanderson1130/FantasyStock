617429671347232d06970105700a9320472f7721
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-09 21:27:56 -0600

    (add) League selected Information

--- src/components/Settings/SettingsLeague.jsx
+++ src/components/Settings/SettingsLeague.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Input, Button
+  Input, Button, Accordion, AccordionSummary, Typography, AccordionDetails
 } from '@material-ui/core';
 import axios from 'axios';
 import AddMembers from './AddMembers.jsx';
@@ -36,13 +36,11 @@ const inputsForm = [
   {
     description: '# of playoff teams',
     type: 'number',
-    placeholder: '# of playoff teams',
     name: 'numberTeamsPlayoffs'
   },
   {
     description: 'starting bank',
     type: 'number',
-    placeholder: 'starting bank',
     name: 'startingBank'
   }
 ];
@@ -59,6 +57,7 @@ function SettingsLeague({ myLeague }) {
   const [leagueForm, setLeagueForm] = useState({});
   const [submitted, setSubmitted] = useState(false);
   const [leagueUsers, setLeagueUsers] = useState([]);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     axios.get(`/league/league/${myLeague.id}`)
@@ -71,44 +70,66 @@ function SettingsLeague({ myLeague }) {
     e.preventDefault();
     setSubmitted(true);
     setTimeout(() => setSubmitted(false), 2000);
+    setLeagueForm({});
+  };
+
+  const handleAccordion = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
   };
 
   return (
     <div className='settingsLeague'>
-      <h2 className='settingsLeague_title'>League Settings</h2>
-      <h3 className='settingsLeague_leagueName'>
-        {`League Name: ${myLeague?.league_name}`}
-      </h3>
-      <div>
-        <form className='settingsLeague_form' onSubmit={handleSubmit}>
-          {submitted && <div className='success-message'>Success! Your settings have been updated</div>}
-          {inputsForm.map(({
-            description, type, name
-          }) => (
-            <div
-              className='settingsLeague_settingBox'
-            >
-              <p>{description}</p>
-              <Input
-                key={name}
-                type={type}
-                name={name}
-                onChange={handleChange}
-              />
+      <Accordion
+        square
+        expanded={expanded === 'leagueSettings'}
+        onChange={handleAccordion('leagueSettings')}
+      >
+        <AccordionSummary
+          aria-controls='panel1d-content'
+          id='panel1d-header'
+        >
+          <Typography className='leagueInfo_members'>
+            League Settings
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <h3 className='settingsLeague_leagueName'>
+              {`League Name: ${myLeague?.league_name}`}
+            </h3>
+            <div>
+              <form className='settingsLeague_form' onSubmit={handleSubmit}>
+                {submitted && <div className='success-message'>Success! Your settings have been updated</div>}
+                {inputsForm.map(({
+                  description, type, name
+                }) => (
+                  <div
+                    className='settingsLeague_settingBox'
+                  >
+                    <p>{description}</p>
+                    <Input
+                      key={name}
+                      type={type}
+                      name={name}
+                      onChange={handleChange}
+                    />
+                  </div>
+                ))}
+                <Button
+                  className='settingsLeague_formButton'
+                  variant='contained'
+                  color='primary'
+                  type='submit'
+                >
+                  Submit
+                </Button>
+              </form>
             </div>
-          ))}
-          <Button
-            className='settingsLeague_formButton'
-            variant='contained'
-            color='primary'
-            type='submit'
-          >
-            Submit
-          </Button>
-        </form>
-        <div className='settingsLeague_addMembers'>
-          <AddMembers leagueUsers={leagueUsers} setLeagueUsers={setLeagueUsers} />
-        </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <div className='settingsLeague_addMembers'>
+        <AddMembers leagueUsers={leagueUsers} setLeagueUsers={setLeagueUsers} />
       </div>
     </div>
   );
