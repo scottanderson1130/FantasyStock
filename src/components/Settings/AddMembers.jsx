a65a6254d2ba80ff32e3b38e14b93c828ab30556
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-11-09 21:29:28 -0600

    (add) according to add members option

--- src/components/Settings/AddMembers.jsx
+++ src/components/Settings/AddMembers.jsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import propTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { Button, Input } from '@material-ui/core';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Input,
+  Typography
+} from '@material-ui/core';
 import { selectLeague } from '../../features/leagueSlice.js';
 import '../../css/AddMembers.css';
 
@@ -18,6 +25,7 @@ function AddMembers({ leagueUsers }) {
   const [users, setUsers] = useState(leagueUsers);
   const leagueID = useSelector(selectLeague);
   const [submitted, setSubmitted] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     setUsers(leagueUsers);
@@ -46,65 +54,86 @@ function AddMembers({ leagueUsers }) {
     setTimeout(() => setSubmitted(false), 1000);
   };
 
+  const handleAccordion = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
     <div>
-      <h2 className='addMembers_title'>Add Members</h2>
-      <p className='addMembers_membersNumber'>
-        {`members: ${users.length}`}
-      </p>
-      <form className='addMembers_form'>
-        <Input
-          variant='outlined'
-          type='text'
-          value={input}
-          onChange={handleChange}
-        />
-        <Button
-          variant='contained'
-          color='primary'
-          type='submit'
-          onClick={addMembers}
-          disabled={!input}
+      <Accordion
+        square
+        expanded={expanded === 'leagueSettings'}
+        onChange={handleAccordion('leagueSettings')}
+      >
+        <AccordionSummary
+          aria-controls='panel1d-content'
+          id='panel1d-header'
         >
-          add
-        </Button>
-      </form>
+          <Typography className='leagueInfo_members'>
+            Add Members
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p className='addMembers_membersNumber'>
+              {`members: ${users.length}`}
+            </p>
+            <form className='addMembers_form'>
+              <Input
+                variant='outlined'
+                type='text'
+                value={input}
+                onChange={handleChange}
+              />
+              <Button
+                variant='contained'
+                color='primary'
+                type='submit'
+                onClick={addMembers}
+                disabled={!input}
+              >
+                add
+              </Button>
+            </form>
 
-      <ol className='addMembers_ol'>
-        {users.map((user) => (
-          <div className='addMembers_newList'>
-            <li className='addMember_username' key={user.id}>
-              {user.username}
-              <p className='addMember_fullName'>{user.full_name}</p>
-            </li>
-            <Button
-              className='addMembers_deleteButton'
-              variant='contained'
-              color='primary'
-              type='button'
-              onClick={() => deleteSelection(user.id)}
-            >
-              delete
-            </Button>
-          </div>
-        ))}
-      </ol>
-      {
-        users.length > 0
-        && (
-          <div className='addMembers_addMembersButton'>
-            <Button
-              variant='contained'
-              color='primary'
-              type='submit'
-              onClick={addMembersToLeague}
-            >
-              Add Members to League
-            </Button>
-          </div>
-        )
-      }
-      {submitted && <p className='addMembers_saved'>Saved</p>}
+            <ol className='addMembers_ol'>
+              {users.map((user) => (
+                <div className='addMembers_newList'>
+                  <li className='addMember_username' key={user.id}>
+                    {user.username}
+                    <p className='addMember_fullName'>{user.full_name}</p>
+                  </li>
+                  <Button
+                    className='addMembers_deleteButton'
+                    variant='contained'
+                    color='primary'
+                    type='button'
+                    onClick={() => deleteSelection(user.id)}
+                  >
+                    delete
+                  </Button>
+                </div>
+              ))}
+            </ol>
+            {
+              users.length > 0
+              && (
+                <div className='addMembers_addMembersButton'>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    type='submit'
+                    onClick={addMembersToLeague}
+                  >
+                    Add Members to League
+                  </Button>
+                </div>
+              )
+            }
+            {submitted && <p className='addMembers_saved'>Saved</p>}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
